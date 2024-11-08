de24808d1b4cb33d2ad1622a33268633542bce0a
Rabiu Ahmad O. <rabiu2993@gmail.com>
2024-11-08 21:35:12 +0100

    Add simple starting middleware

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const express = require(`express`);
+// const Joi = require(`joi`);
+const logger = require(`./utils/logger`);
+const authenticate = require(`./middlewares/authenticate`);
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(express.json());
+
+app.use(logger);
+
+app.use(authenticate)
+
+const genres = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Horror" },
+  { id: 3, name: "Comedy" },
+  { id: 4, name: "Drama" },
+  { id: 5, name: "Sci-Fi" },
+];
+
+app.get(`/`, (req, res) => {
+  res.send(`Hello World!`);
+});
+
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
