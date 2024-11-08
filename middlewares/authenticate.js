3ec4d79918fb3491799090d9fd2d055571cc7531
Rabiu Ahmad O. <rabiu2993@gmail.com>
2024-11-08 22:38:12 +0100

    Refacto vidly setup code.

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,5 +1,4 @@
 const authenticate = (req, res, next) => {
-  console.log("Authenticating...");
   next();
 };
 
--- server.js
+++ server.js
@@ -1,41 +1,19 @@
-const config = require(`config`);
 const express = require(`express`);
 const logger = require(`./utils/logger`);
-const authenticate = require(`./middlewares/authenticate`);
 const helmet = require("helmet");
-const morgan = require("morgan");
-// const Joi = require(`joi`);
+const genres = require(`./routes/genres`);
+const root = require(`./routes/root`);
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(`public`));
 app.use(logger);
-if (app.get("env") === "development") app.use(morgan("tiny"));
-
 app.use(helmet());
 
-console.log(`Application Name: ${config.get(`name`)}`);
+app.use("/api/genres", genres);
 
-app.use(authenticate);
-
-const genres = [
-  { id: 1, name: "Action" },
-  { id: 2, name: "Horror" },
-  { id: 3, name: "Comedy" },
-  { id: 4, name: "Drama" },
-  { id: 5, name: "Sci-Fi" },
-];
-
-app.get(`/`, (req, res) => {
-  res.send(`Hello World!`);
-});
-
-app.get("/api/genres", (req, res) => {
-  res.send(genres);
-});
+app.get(`/`, root);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
