e50947071270f5a061a496b3ebb313cfdb57ebbf
Rabiu Ahmad O. <rabiu2993@gmail.com>
2024-11-09 21:32:41 +0100

    Fix: Movie validation error

--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { index, post, show, update } from "../controllers/movie.js";
+import { index, post, remove, show, update } from "../controllers/movie.js";
 import validateMovie from "../validations/movie.js";
 
 const router = Router();
@@ -8,5 +8,6 @@ router.get("/", index);
 router.get("/:id", show);
 router.post("/", validateMovie, post);
 router.put("/:id", validateMovie, update);
+router.delete("/:id", remove);
 
 export default router;
--- validations/movie.js
+++ validations/movie.js
@@ -5,12 +5,12 @@ const validateMovie = (req, res, next) => {
   const schema = Joi.object({
     title: Joi.string().min(0).max(255).required(),
     genreId: Joi.string().min(1).max(50).required(),
-    numberInStock: Joi.number().min(0).max(255).default(0),
-    dailyRentalRate: Joi.number().min(0).max(255).default(0),
+    numberInStock: Joi.number().min(0).max(255).default(0).optional(),
+    dailyRentalRate: Joi.number().min(0).max(255).default(0).optional(),
   });
 
-  const { errors } = schema.validate(req.body);
-  if (errors) return res.status(400).json(err(errors.details[0].message));
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).json(err(error.details[0].message));
 
   return next();
 };
