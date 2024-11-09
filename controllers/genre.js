752ad22709e539b704cdee742b97110d765fb7e1
Rabiu Ahmad O. <rabiu2993@gmail.com>
2024-11-09 18:14:28 +0100

    Add: Seperate business logic into controllers.

---
+++ controllers/genre.js
@@ -0,0 +1,55 @@
+import {
+  deleteGenre,
+  getGenre,
+  getGenres,
+  postGenre,
+  updateGenre,
+} from "../services/genre";
+import error from "../utils/error";
+import _res from "../utils/response";
+
+export const index = async (req, res) => {
+  const genres = await getGenres();
+
+  res
+    .status(200)
+    .json(_res({ message: "Genres fetched successfully", data: genres }));
+};
+
+export const show = async (req, res) => {
+  const genre = await getGenre(req.params.id);
+
+  if (!genre)
+    return res
+      .status(404)
+      .send(error("The genre with the given ID was not found."));
+
+  res
+    .status(200)
+    .json(_res({ message: "Genre fetched successfully", data: genre }));
+};
+
+export const post = (req, res) => {
+  const newGenre = postGenre(req.body);
+
+  res
+    .status(200)
+    .json(_res({ message: "Genre added successfully", data: newGenre }));
+};
+
+export const update = async (req, res) => {
+  const genre = await updateGenre(req.params.id, req.body);
+
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
+
+  res
+    .status(200)
+    .json(_res({ message: "Genre fetched successfully", data: genre }));
+};
+
+export const remove = async (req, res) => {
+  await deleteGenre(req.params.id);
+
+  res.status(200).json({ message: "Genre deleted successfully" });
+};
--- routes/genres.js
+++ routes/genres.js
@@ -1,65 +1,12 @@
-import Joi from "joi";
 import { Router } from "express";
-import {
-  deleteGenre,
-  getGenre,
-  getGenres,
-  postGenre,
-  updateGenre,
-} from "../services/genre.js";
-import error from "../utils/error.js";
-import _res from "../utils/response.js";
-import validateGenre from "../validations/genre.js";
+import { index, post, remove, show, update } from "../controllers/genre.js";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-  const genres = await getGenres();
-
-  res
-    .status(200)
-    .json(_res({ message: "Genres fetched successfully", data: genres }));
-});
-
-router.get("/:id", async (req, res) => {
-  const genre = await getGenre(req.params.id);
-
-  if (!genre)
-    return res
-      .status(404)
-      .send(error("The genre with the given ID was not found."));
-
-  res
-    .status(200)
-    .json(_res({ message: "Genre fetched successfully", data: genre }));
-});
-
-router.post("/", validateGenre, (req, res, next) => {
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const newGenre = postGenre(req.body);
-
-  res
-    .status(200)
-    .json(_res({ message: "Genre added successfully", data: newGenre }));
-});
-
-router.put("/:id", validateGenre, async (req, res) => {
-  const genre = await updateGenre(req.params.id, req.body);
-
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
-
-  res
-    .status(200)
-    .json(_res({ message: "Genre fetched successfully", data: genre }));
-});
-
-router.delete("/:id", async (req, res) => {
-  await deleteGenre(req.params.id);
-
-  res.status(200).json({ message: "genre deleted successfully" });
-});
+router.get("/", index);
+router.get("/:id", show);
+router.post("/", validateGenre, post);
+router.put("/:id", validateGenre, update);
+router.delete("/:id", remove);
 
 export default router;
