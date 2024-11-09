8a1faedf26d4c3af94d55aa25498553052a2fa1e
Rabiu Ahmad O. <rabiu2993@gmail.com>
2024-11-09 16:11:01 +0100

    Add: Genre service functions

--- services/genre.js
+++ services/genre.js
@@ -2,50 +2,30 @@ import Genre from "../models/genre.js";
 import { GenreResource, GenresResource } from "../resources/genres.js";
 
 export const getGenres = async () => {
-  try {
-    const genres = await Genre.find({}).select({ name: 1 });
+  const genres = await Genre.find({}).select({ name: 1 }).sort({ name: 1 });
 
-    return GenresResource(genres);
-  } catch (error) {
-    throw new Error(error);
-  }
+  return GenresResource(genres);
 };
 
 export const getGenre = async (id) => {
-  try {
-    const genre = await Genre.findById(id).select({ name: 1 });
+  const genre = await Genre.findById(id).select({ name: 1 });
 
-    return GenreResource(genre);
-  } catch (error) {
-    throw new Error(error);
-  }
+  return GenreResource(genre);
 };
 
 export const postGenre = async (data) => {
-  try {
-    const newGenre = new Genre(data);
-    const savedGenre = await newGenre.save();
+  const newGenre = new Genre(data);
+  const savedGenre = await newGenre.save();
 
-    return GenreResource(savedGenre);
-  } catch (error) {
-    throw new Error(error);
-  }
+  return GenreResource(savedGenre);
 };
 
 export const updateGenre = async (id, data) => {
-  try {
-    const genre = await Genre.findByIdAndUpdate(id, data, { new: true });
+  const genre = await Genre.findByIdAndUpdate(id, data, { new: true });
 
-    return GenreResource(genre);
-  } catch (error) {
-    throw new Error(error);
-  }
+  return GenreResource(genre);
 };
 
 export const deleteGenre = async (id) => {
-  try {
-    return await Genre.findByIdAndDelete(id);
-  } catch (error) {
-    throw new Error(error);
-  }
+  return await Genre.findByIdAndDelete(id);
 };
