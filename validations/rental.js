fcaff0dd90caab9ddd3336335c1a74a39c59a28c
Rabiu Ahmad O. <rabiu2993@gmail.com>
2024-11-10 00:33:58 +0100

    Fix: Improve rental validation

--- validations/rental.js
+++ validations/rental.js
@@ -1,10 +1,32 @@
 import Joi from "joi";
 import err from "../utils/error.js";
+import Movie from "../models/movie.js";
+import Customer from "../models/customer.js";
 
 const validateRental = (req, res, next) => {
   const schema = Joi.object({
-    customerId: Joi.string().min(5).max(50).required(),
-    movieId: Joi.string().min(5).max(50).required(),
+    customerId: Joi.string()
+      .min(5)
+      .max(50)
+      .required()
+      .custom(async (value, helpers) => {
+        const customerExists = await checkCustomerExistsInDB(value);
+        if (!customerExists) {
+          return helpers.message("Customer ID is invalid or does not exist");
+        }
+        return value;
+      }),
+    movieId: Joi.string()
+      .min(5)
+      .max(50)
+      .required()
+      .custom(async (value, helpers) => {
+        const movieExists = await checkMovieExistsInDB(value);
+        if (!movieExists) {
+          return helpers.message("Movie ID is invalid or does not exist");
+        }
+        return value;
+      }),
   });
 
   const { error } = schema.validate(req.body);
@@ -12,5 +34,15 @@ const validateRental = (req, res, next) => {
 
   return next();
 };
+
+async function checkCustomerExistsInDB(customerId) {
+  const customer = await Customer.findById(customerId);
+  return !!customer;
+}
+
+async function checkMovieExistsInDB(movieId) {
+  const movie = await Movie.findById(movieId);
+  return !!movie || movie.numberInStock > 0;
+}
 
 export default validateRental;
